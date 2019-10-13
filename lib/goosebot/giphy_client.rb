eaa875f6a669857753fe1f78961864a12fc9e551
Graham Marlow <mgmarlow@gmail.com>
2019-10-13 16:13:06 -0700

    Make nsfw

--- lib/goosebot/giphy_client.rb
+++ lib/goosebot/giphy_client.rb
@@ -2,27 +2,44 @@ module Goosebot
   class GiphyClient
     attr_accessor :client, :api_key
 
+    FOODS = %w[
+      burrito
+      taco
+      pizza
+      doughnut
+      grapefruit
+      melons
+      'taco bell'
+    ]
+
+    MISC = %w[
+      'athletic butt'
+      butts
+      blonde
+      'female gymnast'
+    ]
+
     def initialize
       @client  = ::GiphyClient::DefaultApi.new
       @api_key = ENV['GIPHY_API_KEY']
     end
 
     def call
-      client.gifs_random_get(api_key, search_options)
+      client.gifs_search_get(api_key, get_tag, search_options)
     end
 
     private
 
     def search_options
       {
-        tag: get_tag,
         rating: 'r',
         lang: 'en'
       }
     end
 
     def get_tag
-      if rand(2) == 0 then 'food' else 'hot girl' end
+      items = FOODS.concat(MISC)
+      items[rand(items.count)]
     end
   end
 end
